0fe02ff5e0f0911129475ac3631ea105bd2f0b5f
superman1408 <superman14082017@gmail.com>
2025-07-28 10:17:19 +0530

    Response message from server is changed in auth-controller.js.

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
 
+
 const registerUser = async (req, res) => {
 
 
@@ -11,7 +12,7 @@ const registerUser = async (req, res) => {
         const existingUser = await User.findOne({ email });
 
         if (existingUser) {
-            return res.status(400).json({ message: "User already exist."})
+            return res.status(400).json({ message: "Email address already in use."})
         }
 
         const salt = await bcrypt.genSalt(10);
@@ -24,11 +25,11 @@ const registerUser = async (req, res) => {
             name,
         });
 
-        // TODO send email.
+        // TODO: send email.
 
         
         res.status(201).json({
-            message: "Verification email is send to your. Please check and verify your account."
+            message: "Verification email is send to your email. Please check and verify your account."
         });
         
     } catch (error) {
