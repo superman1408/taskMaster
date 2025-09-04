812beb73c5b075653dfc535f208d6cbd7b2b4ef9
superman1408 <superman14082017@gmail.com>
2025-09-04 16:09:18 +0530

    Project page is created with all the detail and task can be created and other information is available.

--- backend/controllers/task.js
+++ backend/controllers/task.js
@@ -2,6 +2,9 @@ import Project from "../models/project.js";
 import Task from "../models/task.js";
 import Workspace from "../models/workspace.js";
 
+
+
+//............Creating Task ...........................................................
 const createTask = async (req, res) => { 
     console.log("Create Task");
     try {
@@ -60,5 +63,47 @@ const createTask = async (req, res) => {
 };
 
 
+//...........Receiving Task sorted with ID to frontend through this function..................
+const getTaskById = async (req, res) => { 
+    console.log("You triggered get task by ID in backend");
 
-export { createTask };
+
+    try {
+        const { taskId } = req.params;
+
+        const task = await Task.findById(taskId)
+            .populate("assignees", "name profilePicture")
+            .populate("watchers", "name profilePicture");
+        
+        
+        if (!task) {
+            return res.status(404).json({
+                message: "Task not found",
+            });
+        }
+
+
+        const project = await Project.findById(task.project)
+            .populate(
+                "members.user",
+                "name profilePicture"
+        );
+        
+        res.status(200).json({
+            task,
+            project
+        });
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: "Internal server error",
+        });
+        
+    }
+    
+};
+
+
+
+export { createTask, getTaskById };
